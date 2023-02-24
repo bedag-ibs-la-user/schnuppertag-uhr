1ee656f9c9d2d0cf6787195710185bdc4f3bd8eb
bedag-ibs-la-user <79101505+bedag-ibs-la-user@users.noreply.github.com>
2023-02-24 10:20:47 +0100

    update with function

--- sketch.js
+++ sketch.js
@@ -1,46 +1,32 @@
-/* eslint-disable no-undef, no-unused-vars */
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // background(100);
   frameRate(60); // default value = display refresh rate
 }
 
 function draw() {
-  //fill(random(255), random(255), random(255));
-  //rect(5, 5, random(395), random(395));
-
   //Stunden
   let winkel = (hour() / 12.0) * TWO_PI;
-  fill(255, 255, 0);
-  arc(200, 200, 350, 350, 0 - HALF_PI, winkel - HALF_PI);
-
-  fill(255, 120, 0);
-  arc(200, 200, 350, 350, winkel - HALF_PI, TWO_PI - HALF_PI);
+  drawPart(winkel, 350, "#FFFF00", "#FF7800");
 
   //Minuten
   winkel = (minute() / 60.0) * TWO_PI;
-  fill(0, 255, 255);
-  arc(200, 200, 250, 250, 0 - HALF_PI, winkel - HALF_PI);
-
-  fill(0, 120, 255);
-  arc(200, 200, 250, 250, winkel - HALF_PI, TWO_PI - HALF_PI);
+  drawPart(winkel, 250, "#00FFFF", "#0078FF");
 
   //Sekunden
   winkel = (second() / 60.0) * TWO_PI;
-  fill(150, 255, 200);
-  arc(200, 200, 150, 150, 0 - HALF_PI, winkel - HALF_PI);
-
-  fill(100, 120, 255);
-  arc(200, 200, 150, 150, winkel - HALF_PI, TWO_PI - HALF_PI);
+  drawPart(winkel, 150, "#96FFC8", "#6478FF");
 
   //Millisekunden
   winkel = ((millis() % 1000) / 1000.0) * TWO_PI;
-  fill(230, 255, 100);
-  arc(200, 200, 100, 100, 0 - HALF_PI, winkel - HALF_PI);
+  drawPart(winkel, 100, "#E6FF64", "#967850");
+}
 
-  fill(150, 120, 80);
-  arc(200, 200, 100, 100, winkel - HALF_PI, TWO_PI - HALF_PI);
+function drawPart(winkel, diameter, background, foreground) {
+  fill(background);
+  arc(200, 200, diameter, diameter, 0 - HALF_PI, winkel - HALF_PI);
+
+  fill(foreground);
+  arc(200, 200, diameter, diameter, winkel - HALF_PI, TWO_PI - HALF_PI);
 }
 
 // This Redraws the Canvas when resized
